775e900d13a59de0d6f711949a466de27059cedc
Mr. Jack <maesongamer@gmail.com>
2020-01-19 15:53:44 -0500

    cambiosBASE

--- db.sql
+++ db.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE miBase;
 CREATE TABLE cartillas2(
    ID SERIAL PRIMARY KEY,
-   info JSON NOT NULL
+   info JSON NOT NULL,
+	Ganador TEXT
 )
-insert into books values (,'La leyenda del Dragon','Pepito')
+CREATE TABLE numLanzados(
+   ID SERIAL PRIMARY KEY,
+   lanzados JSON NOT NULL
+)
